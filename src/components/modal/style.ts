589d289eae0190e9137de76473dfa170d836535a
toni500madmax <toni500madmax@gmail.com>
2025-06-05 12:39:32 -0300

    fix: modal backdrop background-color adjust

--- src/components/modal/style.ts
+++ src/components/modal/style.ts
@@ -11,7 +11,7 @@ export const ModalContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.25);
     z-index: 1000;
   }
 `;
