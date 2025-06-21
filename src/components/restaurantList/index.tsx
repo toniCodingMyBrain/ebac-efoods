d2fe84147a9b99ab93131a33e985431fbfcbebe0
toni500madmax <toni500madmax@gmail.com>
2025-06-21 13:19:48 -0300

    fix: ensure data is fetching as its type

--- src/components/restaurantList/index.tsx
+++ src/components/restaurantList/index.tsx
@@ -1,4 +1,5 @@
 import { useGetRestaurantsQuery } from "../../services/api";
+import { Restaurant } from "../../services/restaurants-types";
 import { RestaurantCard } from "../card";
 import { List } from "./style";
 
@@ -8,32 +9,32 @@ export type RestaurantListProps = {
 
 export const RestaurantList = ({ type }: RestaurantListProps) => {
   // const { restaurants } = useRestaurantsContext();
-  const { data: restaurants, isLoading } = useGetRestaurantsQuery();
+  const { data, isLoading } = useGetRestaurantsQuery();
+  const restaurants = data as Restaurant[];
 
-  if (restaurants || !isLoading) {
-    return (
-      <div>
-        <List type={type}>
-          {restaurants.map((restaurant) => (
-            <>
-              <RestaurantCard
-                key={restaurant.id}
-                type={type}
-                foodId={restaurant.id}
-                foodName={restaurant.titulo}
-                description={restaurant.descricao}
-                tag={restaurant.destacado ? ["Destaque", restaurant.tipo] : [restaurant.tipo]}
-                destacado={restaurant.destacado}
-                image={restaurant.capa}
-                rating={restaurant.avaliacao}
-                to={`/${restaurant.tipo.toLowerCase()}`}
-              />
-            </>
-          ))}
-        </List>
-      </div>
-    );
-  } else {
-    return <h4>Carregando...</h4>;
-  }
+  if (!isLoading) return <h4>Carregando...</h4>;
+  if (!data) return <h4>Erro ao carregar os dados...</h4>;
+
+  return (
+    <div>
+      <List type={type}>
+        {restaurants.map((restaurant) => (
+          <>
+            <RestaurantCard
+              key={restaurant.id}
+              type={type}
+              foodId={restaurant.id}
+              foodName={restaurant.titulo}
+              description={restaurant.descricao}
+              tag={restaurant.destacado ? ["Destaque", restaurant.tipo] : [restaurant.tipo]}
+              destacado={restaurant.destacado}
+              image={restaurant.capa}
+              rating={restaurant.avaliacao}
+              to={`/${restaurant.tipo.toLowerCase()}`}
+            />
+          </>
+        ))}
+      </List>
+    </div>
+  );
 };
