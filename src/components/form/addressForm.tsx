263d5406e4ff13b4652f89f81fc8fb8da9b4707b
toni500madmax <toni500madmax@gmail.com>
2025-07-26 12:32:49 -0300

    feat: address component update

--- src/components/form/addressForm.tsx
+++ src/components/form/addressForm.tsx
@@ -1,9 +1,39 @@
-import { AddressContainer } from "./style";
+import { AddressContainer, RowBlock } from "./style";
 
 export const StepAddressForm = () => {
   return (
     <AddressContainer>
-      <h4>Step Address Form</h4>
+      <h4>Entrega</h4>
+      <form>
+        <div>
+          <RowBlock inputType="normal">
+            <label htmlFor="fullName">Quem irá receber</label>
+            <input id="fullName" type="text" />
+          </RowBlock>
+          <RowBlock inputType="normal">
+            <label htmlFor="address">Endereço</label>
+            <input id="address" type="text" />
+          </RowBlock>
+          <RowBlock inputType="normal">
+            <label htmlFor="city">Cidade</label>
+            <input id="city" type="text" />
+          </RowBlock>
+          <RowBlock inputType="double">
+            <div className="fields">
+              <label htmlFor="cep">CEP</label>
+              <input id="cep" type="text" />
+            </div>
+            <div className="fields">
+              <label htmlFor="number">Número</label>
+              <input id="number" type="text" />
+            </div>
+          </RowBlock>
+          <RowBlock inputType="normal">
+            <label htmlFor="description">Complemento</label>
+            <input id="description" type="text" />
+          </RowBlock>
+        </div>
+      </form>
     </AddressContainer>
   );
 };
--- src/components/form/style.ts
+++ src/components/form/style.ts
@@ -1,10 +1,67 @@
 import styled from "styled-components";
 import { colors } from "../../GlobalStyle";
 
+export type FormProps = {
+  inputType: "normal" | "double";
+};
+
 export const AddressContainer = styled.div`
-  h4,
-  p,
-  label {
-    color: ${colors.white};
+  display: flex;
+  flex-direction: column;
+  color: ${colors.lightBeige};
+
+  h4 {
+    margin-bottom: 16px;
+    font-size: 16px;
+    font-weight: bold;
   }
+`;
+
+export const RowBlock = styled.div<FormProps>`
+  margin-bottom: 8px;
+
+  label {
+    margin-bottom: 8px;
+    font-size: 14px;
+    font-weight: 600;
+  }
+
+  input {
+    height: 32px;
+    font-size: 14px;
+    color: #4b4b4b;
+    outline: none;
+    border: 3px solid ${colors.rose};
+    border-radius: 2px;
+    padding: 6px 0 6px 0;
+
+    &:focus {
+      border-color: ${colors.beige};
+    }
+  }
+
+  ${(formProps) =>
+    formProps.inputType === "normal"
+      ? `
+      display: flex;
+      flex-direction: column;
+
+      input {
+        width: 100%;
+      }
+    `
+      : `
+      display: flex;
+      gap: 34px;
+
+      .field {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+      }
+
+      input {
+        width: 100%;
+      }
+  `}
 `;
